fdea9f69899d36750e386dcbcfbc81b06cd9dec9
msalari <msalari@sfu.ca>
2020-05-28 20:08:51 -0700

    Change dataset from STL10 to CIFAR10

--- data_aug/dataset_wrapper.py
+++ data_aug/dataset_wrapper.py
@@ -20,8 +20,10 @@ class DataSetWrapper(object):
     def get_data_loaders(self):
         data_augment = self._get_simclr_pipeline_transform()
 
-        train_dataset = datasets.STL10('./data', split='train+unlabeled', download=True,
-                                       transform=SimCLRDataTransform(data_augment))
+        # train_dataset = datasets.STL10('./data', split='train+unlabeled', download=True,
+        #                                transform=SimCLRDataTransform(data_augment))
+        train_dataset = datasets.CIFAR10('./data/cifar10', download=True, train=True, 
+                                         transform=SimCLRDataTransform(data_augment))
 
         train_loader, valid_loader = self.get_train_validation_data_loaders(train_dataset)
         return train_loader, valid_loader
